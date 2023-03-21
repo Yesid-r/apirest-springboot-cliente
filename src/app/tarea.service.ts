9402132b799a14d42b483a4d915226bb6d45610c
Yesid-r <dairo.rincon@uptc.edu.co>
2023-03-20 20:17:13 -0500

    Finish CRUD

--- src/app/tarea.service.ts
+++ src/app/tarea.service.ts
@@ -15,10 +15,10 @@ export class TareaService {
   create(tarea:any){
     return this.http.post('http://localhost:8080/api/tareas', tarea);
   }
-  update(id:number, tarea:any){
-    return this.http.put('http://localhost:8080/api/tareas', + id, tarea);
+  update(href:string, tarea:any){
+    return this.http.put(href, tarea);
   }
-  delete(id:number){
-    return this.http.delete('http://localhost:8080/api/tareas'+id);
+  delete( href:string){
+    return this.http.delete(href);
   }
 }
--- src/app/tareas/tareas.component.ts
+++ src/app/tareas/tareas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { TareaService } from '../tarea.service';
 
 @Component({
@@ -6,15 +7,75 @@ import { TareaService } from '../tarea.service';
   templateUrl: './tareas.component.html',
   styleUrls: ['./tareas.component.css']
 })
-export class TareasComponent  implements OnInit{
+export class TareasComponent implements OnInit {
   tareas: any[] = [];
+  formulario: FormGroup = this.formBuilder.group({
+    nombre: [],
+    estado: [false]
+  });
+  tareaEnEdicion:any;
   constructor(
-    private tareaService: TareaService
-  ){}
+    private tareaService: TareaService,
+    private formBuilder: FormBuilder
+  ) { }
   ngOnInit(): void {
-    this.tareaService.getAll().subscribe((tareas: any)=>{
+    this.getAll()
+  }
+  getAll() {
+    this.tareaService.getAll().subscribe((tareas: any) => {
       console.log('tareas', tareas);
       this.tareas = tareas._embedded.tareas;
+    })
+
+  }
+  save() {
+    const values = this.formulario.value;
+    console.log(values)
+    if(this.tareaEnEdicion){
+      this.tareaService.update(this.tareaEnEdicion._links.self.href
+        ,values).subscribe({
+        next: () => {
+          this.getAll();
+          this.tareaEnEdicion= null;
+          this.formulario.setValue({
+            nombre:'',
+            estado:false
+          })
+        }, error: () => {
+  
+        }
+      })
+    }else{
+      this.tareaService.create(values).subscribe({
+        next: () => {
+          this.getAll()
+          this.formulario.setValue({
+            nombre:'',
+            estado:false
+          })
+        }, error: () => {
+  
+        }
+      })
+    }
+  }
+
+  delete(tarea: any) {
+    const ok = confirm('Esta seguro de eliminar esta tarea?');
+    if (ok) {
+      console.log('ruta', tarea._links.self.href);
+      const href = tarea._links.self.href;
+      this.tareaService.delete(href).subscribe(() => {
+        this.getAll();
+      });
+    }
+
+  }
+  edit(tarea: any){
+    this.tareaEnEdicion= tarea;
+    this.formulario.setValue({
+      nombre:tarea.nombre,
+      estado:tarea.estado
     })
   }
 }
